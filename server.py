233605500ceb24b23fe80c3e55edd6331521f6e3
Hubza <33783503+Hubza@users.noreply.github.com>
2022-08-07 13:01:48 +0100

    finish medals templaet

--- server.py
+++ server.py
@@ -5,8 +5,11 @@ import time
 import os
 import textwrap
 import datetime
+import requests
+import json
 from html2image import Html2Image
 hti = Html2Image()
+
 
 def load_template(name):
     html = ""
@@ -14,7 +17,7 @@ def load_template(name):
     path = "templates/" + name
     with open(path + "/index.html", "r") as f:
         html = f.read()
-    
+
     with open("templates/template.css", "r") as f:
         css = f.read()
 
@@ -30,27 +33,45 @@ def render_template(html, css, usetemplate=True):
     hti.screenshot(html_str=html, css_str=css, save_as=path, size=(1280, 720))
     return path
 
+
 def repl_var(html, vars):
     for var in vars:
-        html = html.replace("{" + var[0] + "}", str(var[1]));
-    return html;
+        html = html.replace("{" + var[0] + "}", str(var[1]))
+    return html
+
 
 class Handler(BaseHTTPRequestHandler):
     def respond(path, self):
         with open(path, 'rb') as file:
             self.wfile.write(file.read())
         os.remove(path)
-    
-    def template_medals(self, template):
+
+    def template_medals(self, template, path):
         name = template["name"]
+        medal = path[2]
+
+        url = 'https://osekai.net/medals/api/public/get_medal.php?medal=' + medal
+        resp = requests.get(url)
+        medalinfo = json.loads(resp.text)
+
         template = load_template(name)
-        vars = [["medal_icon", "test"], ["medal_name", "test"], 
-        ["medal_description", "test"], ["medal_hint", "test"], ["medal_solution", "test"], ["accent", "255, 102, 170"]]
-        template["html"] = repl_var(template['html'], vars);
+        vars = [
+            ["medal_icon", medalinfo['Link'].replace(".png", "@2x.png")],
+            ["medal_name", medalinfo['Name']],
+            ["medal_description", medalinfo['Description']],
+            ["medal_hint", medalinfo['Instructions']],
+            ["medal_solution", medalinfo['Solution']],
+            ["accent", "255, 102, 170"]
+        ]
+        template["html"] = repl_var(template['html'], vars)
+
+        if(medalinfo['Instructions'] == "NULL"):
+            template["html"] += "<style>.medalinfo_top_texts h3 { display: none; }</style>"
+
         rendered = render_template(template["html"], template["css"])
         Handler.respond(rendered, self)
 
-    templates = [{"name": "medals", "def":template_medals}]
+    templates = [{"name": "medals", "def": template_medals}]
 
     def do_GET(self):
         path = list(filter(None, self.path.split("/")))
@@ -71,7 +92,7 @@ class Handler(BaseHTTPRequestHandler):
                     self.send_response(200)
                     self.send_header("Content-Type", "image/png")
                     self.end_headers()
-                    template["def"](self, template)
+                    template["def"](self, template, path)
         if(notFound == True):
             self.send_response(404)
             self.end_headers()
